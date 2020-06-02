af2175564f5c18dac3ffab7fbdf7a59517c27161
Say-young <bb8217@naver.com>
2020-06-02 15:34:45 +0900

    Body로 보낼 땐 POST

--- HOMEWORK6/routes/capital.js
+++ HOMEWORK6/routes/capital.js
@@ -2,6 +2,6 @@ var express = require('express');
 var router = express.Router();
 const capitalController = require('../controllers/capital');
 
-router.get('/', capitalController.autoTransfer);
+router.post('/', capitalController.autoTransfer);
 
 module.exports = router;
--- HOMEWORK6/routes/user.js
+++ HOMEWORK6/routes/user.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/user');
 
-router.get('/', userController.showUser);
-router.get('/moneyList', userController.moneyList);
+router.post('/', userController.showUser);
+router.post('/moneyList', userController.moneyList);
 
 module.exports = router;
